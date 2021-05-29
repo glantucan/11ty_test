f24442864aa11036d4dba676af6bf1ed276f0e94
glantucan <glantucan@gmail.com>
2021-05-29 11:57:44 +0100

    Initial config and home and about pages

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+
+var vibes = [
+  'Everbody is great! 🤟',
+  'Mango fruit is great! 🥭',
+  'My mother is great! 💌',
+  'My friends are awsome! ❤️',
+  'Life is great! ⛱️',
+]
+
+setInterval(
+  () => {
+    var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+    document.querySelector(".vibe").innerHTML = null;
+    document.querySelector(".vibe").innerHTML = vibe;
+  },
+  500
+)
